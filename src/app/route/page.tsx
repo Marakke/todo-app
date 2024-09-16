1ba240cce1ff338725f498124de3767b0dd9f9fd
Martti Grönholm <martti.gronholm@vincit.fi>
2024-09-16 13:19:41 +0300

    Add simple routing

--- src/app/route/page.tsx
+++ src/app/route/page.tsx
@@ -1,10 +1,10 @@
-export default function Home() {
+export default function Route() {
   return (
     <main className="m-20">
       <div className="flex flex-col items-center justify-center gap-10">
-        <h1 className="text-4xl">Welcome to my Full Stack Demo Project</h1>
+        <h1 className="text-4xl">Route</h1>
         <p>
-          <a href="/route">To Route</a>
+          <a href="/">To Home</a>
         </p>
       </div>
     </main>
